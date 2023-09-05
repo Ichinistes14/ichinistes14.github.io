a78a8b97cc325c391e819c2cea74fff7a75ae1b0
Ichinistes14 <141771041+Ichinistes14@users.noreply.github.com>
2023-09-05 20:42:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,26 @@
         document.body.classList.toggle("light-mode");
     });
 })();
+
+window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
+
+var form = document.getElementById("contact-form");
+
+form.addEventListener("submit", event => {
+    var data = new FormData(event.target);
+    
+    axios({
+        url: 'https://formspree.io/f/YOUR_FORM_ID',
+        method: 'post',
+        headers: {
+            'Accept': 'application/json'
+        },
+        data: data
+    }).then((response) => {
+        console.log(response);
+    });
+});
