d3c5bd149e683efb9f11267e7108ad564c15f32d
Ichinistes14 <jonathan.devlin35@gmail.com>
2023-09-06 19:11:04 +0200

    Last Version

--- app.js
+++ app.js
@@ -18,20 +18,3 @@ window.onbeforeunload = () => {
     form.reset();
   }
 }
-
-var form = document.getElementById("contact-form");
-
-form.addEventListener("submit", event => {
-    var data = new FormData(event.target);
-    
-    axios({
-        url: 'https://formspree.io/f/mvojrwkj',
-        method: 'post',
-        headers: {
-            'Accept': 'application/json'
-        },
-        data: data
-    }).then((response) => {
-        console.log(response);
-    });
-});
